ccfcfd8491c7c52576cec49b866c1fcffd573ff4
funny-family <maksvadik228@gmail.com>
2020-10-23 12:35:19 +0700

    fix

--- rick_and_morty/fs_task.js
+++ rick_and_morty/fs_task.js
@@ -1,44 +1,36 @@
 const linkFinder = document.querySelector('.link-searcher__input');
 const linkListContainer = document.querySelector('.link-list-container');
 
-const linkList = [].slice.call(document.getElementsByTagName('a'));
+const HTMLLinkList = document.getElementsByTagName('a');
+const linkList = [];
+let filteredLinkList = [];
 
-// const linkItem = document.createElement('li');
+for (let i = 0; i < HTMLLinkList.length; i++) {
+  const href = HTMLLinkList[i].href;
+  const text = HTMLLinkList[i].innerText;
 
-console.log('linkList:', linkList);
-
-// const linkListContainerTemplate = `
-//   <li>${}</li>
-// `;
-
-// https://stackoverflow.com/questions/31829826/print-javascript-array-in-html
-// https://www.google.com/search?ei=Kr6RX5upCOmgrgTz_LmQCg&q=js+print+data+from+array+to+html&oq=js+print+data+from+array+to+h&gs_lcp=CgZwc3ktYWIQAxgAMggIIRAWEB0QHjoECAAQQzoICAAQsQMQgwE6BQgAELEDOgIIADoFCC4QsQM6BwgAELEDEEM6BggAEBYQHjoICAAQFhAKEB5Qs8EJWJ3PCmDW3QpoBnABeACAAcsBiAGeIpIBBjAuMzIuMZgBAKABAaoBB2d3cy13aXrAAQE&sclient=psy-ab
-// https://stackoverflow.com/questions/44127872/convert-array-of-objects-into-html-table-with-jquery-or-javascript
-// https://www.google.com/search?ei=Ub-RX8y3K-_2qwGp3ZiwBA&q=javascript+array+of+objects+to+html+table&oq=js+array+of+objects+in+ht&gs_lcp=CgZwc3ktYWIQAxgBMgYIABAWEB4yBggAEBYQHjoECAAQRzoECAAQQzoCCAA6CAghEBYQHRAeUMQSWJs5YNtYaARwAngAgAGeAYgBxgqSAQQwLjEwmAEAoAEBqgEHZ3dzLXdpesgBCMABAQ&sclient=psy-ab
-
+  linkList.push({
+    href,
+    text,
+    loweCaseText: text.toLowerCase()
+  });
+}
 
 linkFinder.addEventListener('input', (event) => {
-  const inputValue = event.target.value;
+  const inputValue = event.target.value.toLowerCase();
 
-  console.log(inputValue);
+  if (inputValue === '') return;
+
+  filteredLinkList = linkList.filter((link) => {
+    return link.loweCaseText.indexOf(inputValue) >= 0;
+  });
+
+  console.log(filteredLinkList);
 });
 
-for (let i = 0; i < linkList.length; i++) {
-  if (linkList[i].innerText !== '') {
-    linkListContainer.innerHTML = `
-      <li>
-        <a href="${linkList[i].href}">${linkList[i].innerText}</a>
-      </li>
-    `;
+// console.log(linkList); 
 
-    // linkListContainer.innerHTML = linkTag[i].outerHTML;
-
-    // linkListContainer = linkItem.innerHTML = linkTag[i];
-    // linkListContainer.innerHTML =
-
-    // console.log(linkList[i]);
-
-    // console.log();
-    // console.log(i, linkTag[i].innerText);
-  }
-}
+// const filteredLinkList = linkList.filter((elem) => {
+//   //
+// });
+// console.log(filteredLinkList);
