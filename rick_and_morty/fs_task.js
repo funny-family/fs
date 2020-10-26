a344774c2ba1d2b8862264fbde4b348314cab162
funny family <maksvadik228@gmail.com>
2020-10-26 20:20:58 +0700

    fix

--- rick_and_morty/fs_task.js
+++ rick_and_morty/fs_task.js
@@ -1,9 +1,11 @@
-const linkFinder = document.querySelector('.link-searcher__input');
+const linkFinder = document.querySelector('.link-searcher');
 const linkListContainer = document.querySelector('.link-list-container');
+const searchResultField = document.querySelector('.search-result-field');
 
 const HTMLLinkList = document.getElementsByTagName('a');
 const linkList = [];
 let filteredLinkList = [];
+
 let linksFoundResult = 0;
 
 for (let i = 0; i < HTMLLinkList.length; i++) {
@@ -17,28 +19,46 @@ for (let i = 0; i < HTMLLinkList.length; i++) {
   });
 }
 
+// for (let i = 0; i < filteredLinkList.length; i++) {
+//   linkListContainer.innerHTML += `
+//     <li><a href="${filteredLinkList[i].href}">${filteredLinkList[i].text}</a></li>
+//   `;
+// }
+
+function updateLinkList() {
+  for (let i = 0; i < filteredLinkList.length; i++) {
+    searchResultField.innerText = `найдено ${linksFoundResult}`;
+    linkListContainer.innerHTML = `
+      <li><a href="${filteredLinkList[i].href}">${filteredLinkList[i].text}</a></li>
+    `;
+  }
+}
+
 linkFinder.addEventListener('input', (event) => {
   const inputValue = event.target.value.toLowerCase();
 
-  if (inputValue === '') return;
+  if (inputValue === '') {
+    searchResultField.innerText = null;
+    linkListContainer.innerHTML = null;
+    return;
+  }
 
   filteredLinkList = linkList.filter((link) => {
-    return link.loweCaseText.indexOf(inputValue) >= 0;
+    return link.loweCaseText.indexOf(inputValue) >= 0; // first variant
+
+    // return inputValue
+    //   .split('')
+    //   .every((letter) => link.loweCaseText.indexOf(letter) >= 0); // second variant
   });
+
+  updateLinkList();
 
   // for (let i = 0; i < filteredLinkList.length; i++) {
     //   linkListContainer.innerHTML += `
-    //     <li><a herf="${filteredLinkList[i].href}">${filteredLinkList[i].link}</a></li>
+    //     <li><a href="${filteredLinkList[i].href}">${filteredLinkList[i].link}</a></li>
     //   `;
     // }
-    
+
   linksFoundResult = filteredLinkList.length;
   console.log(filteredLinkList, linksFoundResult);
 });
-
-// console.log(linkList); 
-
-// const filteredLinkList = linkList.filter((elem) => {
-//   //
-// });
-// console.log(filteredLinkList);
